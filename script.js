8717deef1dbb1bc9848307653edb5e9d01fbc50f
api <e2202869@edu.vamk.fi>
2023-12-02 15:43:54 +0200

    fixed grammar and clarity

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const text1 = "gcc -o welcome foo.c";
-const text2 = "./welcome";
-const text3 =
+const compile = "gcc -o welcome foo.c";
+const execute = "./welcome";
+const introText =
   "Hi there! My name is Antti. \n\nI'm a 24-year-old IT engineering student at Vaasa UAS.\n" +
-  "Currently studying my second year with a focus on embedded systems.\n\nI'm really intrested in technology," +
-  " especially in Linux operating systems,\n and C/C++ programming. Eager to learn and embrace new challenges.\n\n" +
-  "Feel free to contact me through email, or the social medias linked down below!\n\nKind Regards\nAntti Pirinen";
+  "I am currently in my second year, focusing on embedded systems.\n\nI'm really interested in technology," +
+  " especially in Linux operating systems\n and C/C++ programming. Eager to learn and embrace new challenges.\n\n" +
+  "Feel free to contact me through email, or the social medias linked down below!\n\nKind Regards,\nAntti Pirinen";
+//constants holding "input" elements
 const inputText = document.getElementById("inputText");
 const inputText2 = document.getElementById("inputText2");
+//constant holding element of the second "input" line
 const prompt2 = document.getElementById("prompt2");
 //constant holding textfield elements
 const textField1 = document.getElementById("Q1");
@@ -14,6 +16,8 @@ const textField1 = document.getElementById("Q1");
 const linkElement1 = document.getElementById("link1");
 const linkElement2 = document.getElementById("link2");
 const linkElement3 = document.getElementById("email");
+//const for the email queryselector
+const span = document.querySelector("span");
 
 let counter = 0;
 
@@ -21,12 +25,12 @@ setTimeout(main, 1000);
 
 function main() {
   if (counter === 0) {
-    typeToPrompt(0, text1, inputText, 50);
+    typeToPrompt(0, compile, inputText, 50);
   } else if (counter === 1) {
     showPromtLine();
-    setTimeout(() => typeToPrompt(0, text2, inputText2, 50), 2000);
+    setTimeout(() => typeToPrompt(0, execute, inputText2, 50), 1000);
   } else if (counter === 2) {
-    typeToPrompt(0, text3, textField1, 25);
+    typeToPrompt(0, introText, textField1, 25);
   } else if (counter === 3) {
     typeToPrompt(0, "GitHub", linkElement1, 5);
   } else if (counter === 4) {
@@ -36,6 +40,7 @@ function main() {
   }
 }
 
+//function for the text typing
 function typeToPrompt(index, text, element, speed) {
   if (index < text.length) {
     element.innerHTML += text.charAt(index);
@@ -49,15 +54,12 @@ function typeToPrompt(index, text, element, speed) {
   }
 }
 
-function clearElement() {
-  inputText.innerHTML = "";
-}
-
+//makes the second line visible after the first input
 function showPromtLine() {
   prompt2.style.display = "flex";
 }
 
-const span = document.querySelector("span");
+//these are for copying the email address on click
 
 span.onclick = function () {
   document.execCommand("copy");
