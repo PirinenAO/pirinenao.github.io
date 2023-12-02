41c3864d8133deed3c9dd9cea1f6e3ca5c570e26
api <e2202869@edu.vamk.fi>
2023-12-02 14:11:01 +0200

    few changes

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const text3 =
   "Hi there! My name is Antti. \n\nI'm a 24-year-old IT engineering student from Vaasa UAS.\n" +
   "Currently studying my second year with a focus on embedded systems.\n\nI'm really intrested in technology," +
   " especially in Linux operating systems,\n and C/C++ programming. Eager to learn and embrace new challenges.\n\n" +
-  "Feel free to contact me through email, or the social medias linked down below!\n\nBest Regards\nAntti Pirinen";
-const prompt1text = document.getElementById("launch");
-const prompt2text = document.getElementById("launch2");
+  "Feel free to contact me through email, or the social medias linked down below!\n\nKind Regards\nAntti Pirinen";
+const inputText = document.getElementById("inputText");
+const inputText2 = document.getElementById("inputText2");
 const prompt2 = document.getElementById("prompt2");
 //constant holding textfield elements
 const textField1 = document.getElementById("Q1");
@@ -21,10 +21,10 @@ setTimeout(main, 1000);
 
 function main() {
   if (counter === 0) {
-    typeToPrompt(0, text1, prompt1text, 50);
+    typeToPrompt(0, text1, inputText, 50);
   } else if (counter === 1) {
     showPromtLine();
-    setTimeout(() => typeToPrompt(0, text2, prompt2text, 50), 2000);
+    setTimeout(() => typeToPrompt(0, text2, inputText2, 50), 2000);
   } else if (counter === 2) {
     typeToPrompt(0, text3, textField1, 25);
   } else if (counter === 3) {
@@ -50,7 +50,7 @@ function typeToPrompt(index, text, element, speed) {
 }
 
 function clearElement() {
-  prompt1text.innerHTML = "";
+  inputText.innerHTML = "";
 }
 
 function showPromtLine() {
