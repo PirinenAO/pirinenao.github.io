1ba33cb4ce70a0bb972b1dd9a39abb8cbc8fdefd
api <e2202869@edu.vamk.fi>
2023-12-09 14:08:57 +0200

    added button for skipping the writing animation, and renamed few variables and class names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const compile = "gcc -o introduction foo.c";
-const execute = "./introduction";
+const compileText = "gcc -o introduction foo.c";
+const executeText = "./introduction";
 const introText =
   "Hi there! My name is Antti. \n\nI'm a 24-year-old IT engineering student at Vaasa UAS.\n" +
   "I am currently in my second year, focusing on embedded systems.\n\nI'm really interested in technology," +
@@ -8,8 +8,8 @@ const introText =
 //constants holding "input" elements
 const inputText = document.getElementById("inputText");
 const inputText2 = document.getElementById("inputText2");
-//constant holding element of the second "input" line
-const prompt2 = document.getElementById("prompt2");
+//constant holding element of the second "input" line of terminal
+const line2 = document.getElementById("line2");
 //constant holding textfield elements
 const textField1 = document.getElementById("Q1");
 //constants holding link elements
@@ -18,49 +18,57 @@ const linkElement2 = document.getElementById("link2");
 const linkElement3 = document.getElementById("email");
 //const for the email queryselector
 const span = document.querySelector("span");
+//skip button element
+const skipButton = document.getElementById("skip-button");
 
 let counter = 0;
+let skip = false;
 
-setTimeout(main, 1000);
+setTimeout(setParameters, 1000);
 
-function main() {
-  if (counter === 0) {
-    typeToPrompt(0, compile, inputText, 50);
-  } else if (counter === 1) {
-    showPromtLine();
-    setTimeout(() => typeToPrompt(0, execute, inputText2, 50), 1000);
-  } else if (counter === 2) {
-    typeToPrompt(0, introText, textField1, 25);
-  } else if (counter === 3) {
-    typeToPrompt(0, "GitHub", linkElement1, 5);
-  } else if (counter === 4) {
-    typeToPrompt(0, "LinkedIn", linkElement2, 5);
-  } else if (counter === 5) {
-    typeToPrompt(0, "pirinenao@proton.me", linkElement3, 5);
-  }
-}
-
-//function for the text typing
-function typeToPrompt(index, text, element, speed) {
-  if (index < text.length) {
-    element.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(() => typeToPrompt(index, text, element, speed), speed); // Adjust typing speed (milliseconds)
-
-    if (index === text.length) {
-      counter++;
-      setTimeout(main, 1000);
+// setting parameters for typeToTerminal
+function setParameters() {
+  if (!skip) {
+    if (counter === 0) {
+      typeToScreen(0, compileText, inputText, 50);
+    } else if (counter === 1) {
+      showSecondLine();
+      setTimeout(() => typeToScreen(0, executeText, inputText2, 50), 1000);
+    } else if (counter === 2) {
+      typeToScreen(0, introText, textField1, 25);
+    } else if (counter === 3) {
+      typeToScreen(0, "GitHub", linkElement1, 5);
+    } else if (counter === 4) {
+      typeToScreen(0, "LinkedIn", linkElement2, 5);
+    } else if (counter === 5) {
+      typeToScreen(0, "pirinenao@proton.me", linkElement3, 5);
+      skipButton.style.display = "none";
     }
   }
 }
 
-//makes the second line visible after the first input
-function showPromtLine() {
-  prompt2.style.display = "flex";
+// function for the text typing
+function typeToScreen(index, text, element, speed) {
+  if (!skip) {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(() => typeToScreen(index, text, element, speed), speed); // Adjust typing speed (milliseconds)
+
+      if (index === text.length) {
+        counter++;
+        setTimeout(setParameters, 1000);
+      }
+    }
+  }
+}
+
+// makes the second line appear
+function showSecondLine() {
+  line2.style.display = "flex";
 }
 
 //these are for copying the email address on click
-
 span.onclick = function () {
   document.execCommand("copy");
 };
@@ -71,4 +79,23 @@ span.addEventListener("copy", function (event) {
     event.clipboardData.setData("text/plain", span.textContent);
     console.log(event.clipboardData.getData("text"));
   }
+});
+
+// for skip button
+function skipTyping() {
+  skip = true;
+  inputText.innerHTML = compileText;
+  showSecondLine();
+  inputText2.innerHTML = executeText;
+  textField1.innerHTML = introText;
+  linkElement1.innerHTML = "GitHub";
+  linkElement2.innerHTML = "LinkedIn";
+  linkElement3.innerHTML = "pirinenao@proton.me";
+  skipButton.style.display = "none";
+}
+
+// Find the element, and attach event listener
+document.addEventListener("DOMContentLoaded", function () {
+  var skipButton = document.querySelector(".skip-button");
+  skipButton.addEventListener("click", skipTyping);
 });
